19bf3bd8d1b1cef6c33b47cfa2622530a52d1103
pipe951 <160313757+pipe951@users.noreply.github.com>
2024-12-19 18:48:40 +0700

    Create jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  testResultsProcessor: "jest-junit",  // ใช้ jest-junit สำหรับแปลงผลทดสอบเป็น XML
+  reporters: [
+    "default",  // ใช้ reporter ปกติ
+    [
+      "jest-junit",  // กำหนดให้ jest-junit ใช้ผลทดสอบเป็น XML
+      {
+        outputDirectory: "./test-results",  // ที่เก็บไฟล์ผลทดสอบ
+        outputName: "TESTS-results.xml"     // ชื่อไฟล์ผลทดสอบ
+      }
+    ]
+  ]
+};
