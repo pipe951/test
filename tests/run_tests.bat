e9456ae2e0bb31163179f003a67b362f645ac6e6
pipe951 <160313757+pipe951@users.noreply.github.com>
2024-12-19 18:05:11 +0700

    Update run_tests.bat

--- tests/run_tests.bat
+++ tests/run_tests.bat
@@ -1,5 +1,5 @@
 echo Running Tests...
 :: ตัวอย่างการรัน unit tests โดยใช้ NUnit หรือ JUnit (ต้องติดตั้ง framework ก่อน)
 :: ตัวอย่างการรัน NUnit
-nunit-console tests\*Tests.dll > result.xml
+nunit3-console tests\*Tests.dll > result.xml
 echo Tests completed!
