12e95e59a53e1411e95edd654d264761051d8289
pipe951 <160313757+pipe951@users.noreply.github.com>
2024-12-19 18:26:44 +0700

    Update app.test.js

--- app.test.js
+++ app.test.js
@@ -1,4 +1,4 @@
-const sum = require('./app.js'); // ฟังก์ชันที่เราต้องการทดสอบ
+const sum = require('./script.js'); // ฟังก์ชันที่เราต้องการทดสอบ
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
