2e0b56ae11e2fad570b61a5d360570ac173a4c24
pipe951 <160313757+pipe951@users.noreply.github.com>
2024-12-19 18:24:26 +0700

    Delete tests directory

--- tests/deploy.bat
+++
@@ -1,4 +0,0 @@
-echo Deploying application...
-:: ตัวอย่างการ deploy ไปยัง Server
-copy %BUILD_DIR%\* C:\path\to\deploy
-echo Deploy completed!
--- tests/run_tests.bat
+++
@@ -1,5 +0,0 @@
-echo Running Tests...
-:: ตัวอย่างการรัน unit tests โดยใช้ NUnit หรือ JUnit (ต้องติดตั้ง framework ก่อน)
-:: ตัวอย่างการรัน NUnit
-nunit3-console tests\*Tests.dll > result.xml
-echo Tests completed!
