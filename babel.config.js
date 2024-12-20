80c7cf6c4eed64ba7512bf9acdca1f4290b9b992
pipe951 <160313757+pipe951@users.noreply.github.com>
2024-12-20 18:29:52 +0700

    Create babel.config.js

---
+++ babel.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-react', // สำหรับ React JSX, ถ้าไม่ใช้ React สามารถลบออกได้
+    '@babel/preset-typescript', // หากใช้ TypeScript
+  ],
+};
