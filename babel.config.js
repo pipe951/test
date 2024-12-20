e9637e062149cf98ae47d3d20837d8440ec02ce2
pipe951 <160313757+pipe951@users.noreply.github.com>
2024-12-20 18:46:29 +0700

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,8 @@
-// babel.config.js (ใช้ ESM syntax)
-export default {
+// babel.config.js (ใช้ CommonJS syntax)
+module.exports = {
   presets: [
     '@babel/preset-env',
-    '@babel/preset-react', // สำหรับ React JSX, ถ้าไม่ใช้ React สามารถลบออกได้
+    '@babel/preset-react', // ใช้สำหรับ React JSX, ถ้าไม่ใช้ React สามารถลบออกได้
     '@babel/preset-typescript', // หากใช้ TypeScript
   ],
 };
