a4432c09bc0285bf1fd6264b947f512d4567646b
pipe951 <160313757+pipe951@users.noreply.github.com>
2024-12-20 18:16:06 +0700

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,3 @@
 module.exports = {
-  presets: [
-    '@babel/preset-env',
-    '@babel/preset-react' // หากคุณใช้ React
-  ],
-  plugins: [
-    '@babel/plugin-transform-runtime'
-  ],
+  presets: ['@babel/preset-env'],  // ใช้ @babel/preset-env สำหรับการแปลง JavaScript
 };
