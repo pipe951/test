8b2707beb960220c9add95955d767aa1a04d9c49
pipe951 <160313757+pipe951@users.noreply.github.com>
2024-12-20 19:27:23 +0700

    Update jest.setup.js

--- jest.setup.js
+++ jest.setup.js
@@ -5,3 +5,19 @@ global.localStorage = {
   removeItem: jest.fn(),
   clear: jest.fn()
 };
+
+// ใช้ beforeEach เพื่อตั้งค่า (หรือรีเซ็ต mock) ก่อนการทดสอบแต่ละครั้ง
+beforeEach(() => {
+  localStorage.getItem.mockReset();
+  localStorage.setItem.mockReset();
+  localStorage.removeItem.mockReset();
+  localStorage.clear.mockReset();
+});
+
+// ใช้ afterEach เพื่อล้างข้อมูลหลังการทดสอบแต่ละครั้ง
+afterEach(() => {
+  localStorage.getItem.mockReset();
+  localStorage.setItem.mockReset();
+  localStorage.removeItem.mockReset();
+  localStorage.clear.mockReset();
+});
